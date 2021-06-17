92c69990bd83ce0b2be0f64723b51d26b6eea24a
Jay Shollenberger <jshollen@redhat.com>
2021-06-17 10:59:47 -0600

    echo in curl scripts

--- 2_routing_headers/curl-no-headers.sh
+++ 2_routing_headers/curl-no-headers.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-curl http://istio-ingressgateway-istio-system.apps.ocp4.lab.unixnerd.org/leaf1
+while :
+do
+
+	curl http://istio-ingressgateway-istio-system.apps.ocp4.lab.unixnerd.org/leaf1
+	echo
+	sleep 1
+done
--- 2_routing_headers/curl-with-headers.sh
+++ 2_routing_headers/curl-with-headers.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-curl -H "end-user: redhat" http://istio-ingressgateway-istio-system.apps.ocp4.lab.unixnerd.org/leaf1
+while: 
+do
+   curl -H "end-user: redhat" http://istio-ingressgateway-istio-system.apps.ocp4.lab.unixnerd.org/leaf1
+   sleep 1
+done
